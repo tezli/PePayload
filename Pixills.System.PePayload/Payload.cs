3763ed496f1b21c8f1960c8464d7cae4e97b517c
rtezli <robert@pixills.com>
2013-07-29 21:03:13 +0200

    For the beauty

--- Pixills.System.PePayload/Payload.cs
+++ Pixills.System.PePayload/Payload.cs
@@ -9,76 +9,74 @@ namespace Pixills.System.PePayload
     {
         public static void Append(Stream file, Stream targetStream, string payload)
         {
-            using (var br = new BinaryReader(file))
+            targetStream.SetLength(0);
+            using (var reader = new BinaryReader(file))
+            using (var writer = new BinaryWriter(targetStream))
             {
-                targetStream.SetLength(0);
-                using (var bw = new BinaryWriter(targetStream))
-                {
-                    var mz = br.ReadBytes(2);
-                    if (!mz.SequenceEqual(new byte[] { 0x4d, 0x5a }))
-                        throw new FormatException("The file has no MZ header");
+                var mz = reader.ReadBytes(2);
+                if (!mz.SequenceEqual(new byte[] { 0x4d, 0x5a }))
+                    throw new FormatException("The file has no MZ header");
 
-                    // skip DOS stub and proceed to PE header
-                    br.BaseStream.Seek(0x3c, SeekOrigin.Begin);
-                    var baseOffset = br.ReadByte();
-                    br.BaseStream.Seek(baseOffset, SeekOrigin.Begin);
-                    var peMagic = br.ReadBytes(4);
-                    if (!peMagic.SequenceEqual(new byte[] { 0x50, 0x45, 0x0, 0x0 }))
-                        throw new FormatException("The file has no PE header");
+                // skip DOS stub and proceed to PE header
+                reader.BaseStream.Seek(0x3c, SeekOrigin.Begin);
+                var baseOffset = reader.ReadByte();
+                reader.BaseStream.Seek(baseOffset, SeekOrigin.Begin);
+                var peMagic = reader.ReadBytes(4);
+                if (!peMagic.SequenceEqual(new byte[] { 0x50, 0x45, 0x0, 0x0 }))
+                    throw new FormatException("The file has no PE header");
 
-                    var payloadBytes = Encoding.UTF8.GetBytes(payload);
+                var payloadBytes = Encoding.UTF8.GetBytes(payload);
 
-                    // Skip Coff file header
-                    br.ReadBytes(20);
-                    var magic = br.ReadUInt16();
-                    var isPePlus = magic == 0x20B;
+                // Skip Coff file header
+                reader.ReadBytes(20);
+                var magic = reader.ReadUInt16();
+                var isPePlus = magic == 0x20B;
 
-                    // Skip optional header
-                    br.ReadBytes(isPePlus ? 142 : 126);
+                // Skip optional header
+                reader.ReadBytes(isPePlus ? 142 : 126);
 
-                    // Read address and size of the certificate table
-                    var address = br.ReadUInt32();
-                    var sizePosition = br.BaseStream.Position;
-                    var size = br.ReadUInt32();
-                    if(address == 0)
-                        throw new InvalidOperationException("The file contains no certificate");
+                // Read address and size of the certificate table
+                var address = reader.ReadUInt32();
+                var sizePosition = reader.BaseStream.Position;
+                var size = reader.ReadUInt32();
+                if (address == 0)
+                    throw new InvalidOperationException("The file does not contain any certificate");
 
-                    var newSize = (uint)(size + payloadBytes.Length);
-                    var padding =  8 - (newSize % 8);
-                    newSize += padding;
+                var newSize = (uint)(size + payloadBytes.Length);
+                var padding = 8 - (newSize % 8);
+                newSize += padding;
 
-                    // Copy the first part to the output stream
-                    br.BaseStream.Seek(0, SeekOrigin.Begin);
-                    
-                    var firstChunkbytes = new byte[sizePosition];
-                    br.Read(firstChunkbytes, 0, firstChunkbytes.Length);
-                    bw.Write(firstChunkbytes, 0, firstChunkbytes.Length);
-                    bw.Write(newSize);
+                // Copy the first part to the output stream
+                reader.BaseStream.Seek(0, SeekOrigin.Begin);
 
-                    // reread the size
-                    br.ReadUInt32();
-                    var secondChunkposition = br.BaseStream.Position;
+                var firstChunkbytes = new byte[sizePosition];
+                reader.Read(firstChunkbytes, 0, firstChunkbytes.Length);
+                writer.Write(firstChunkbytes, 0, firstChunkbytes.Length);
+                writer.Write(newSize);
 
-                    // Copy the second part to the output stream
-                    var secondChunkbytes = new byte[address - secondChunkposition];
-                    br.Read(secondChunkbytes, 0, secondChunkbytes.Length);
-                    bw.Write(secondChunkbytes, 0, secondChunkbytes.Length);
-                    
-                    
-                    // Skip the length field in the certificate table header
-                    br.ReadUInt32();
-                    var certTablebytes = new byte[size - 4];
-                    br.Read(certTablebytes, 0, certTablebytes.Length);
-                    // Write the new length to the certificate table header
-                    bw.Write(newSize);
-                    bw.Write(certTablebytes, 0, certTablebytes.Length);
+                // reread the size
+                reader.ReadUInt32();
+                var secondChunkposition = reader.BaseStream.Position;
 
-                    // Copy the payload to output stream
-                    bw.Write(payloadBytes, 0, payloadBytes.Length);
+                // Copy the second part to the output stream
+                var secondChunkbytes = new byte[address - secondChunkposition];
+                reader.Read(secondChunkbytes, 0, secondChunkbytes.Length);
+                writer.Write(secondChunkbytes, 0, secondChunkbytes.Length);
 
-                    // Padd with zeros
-                    bw.Write(new byte[(int)padding], 0, (int)padding);
-                }
+
+                // Skip the length field in the certificate table header
+                reader.ReadUInt32();
+                var certTablebytes = new byte[size - 4];
+                reader.Read(certTablebytes, 0, certTablebytes.Length);
+                // Write the new length to the certificate table header
+                writer.Write(newSize);
+                writer.Write(certTablebytes, 0, certTablebytes.Length);
+
+                // Copy the payload to output stream
+                writer.Write(payloadBytes, 0, payloadBytes.Length);
+
+                // Padd with zeros
+                writer.Write(new byte[(int)padding], 0, (int)padding);
             }
         }
     }
